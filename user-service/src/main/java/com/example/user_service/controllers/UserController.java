55cb515a5bc086249e4ee93ec76db3054385b63b
Ratnasai_Thadikonda <ratnasai.t@digitelenetworks.com>
2025-09-18 23:13:35 +0530

    send and receive data from kafka

--- user-service/src/main/java/com/example/user_service/controllers/UserController.java
+++ user-service/src/main/java/com/example/user_service/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.example.user_service.controllers;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,8 +29,27 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String createUser(@RequestBody Map<String, String> user) {
-        kafkaTemplate.send("user-events", "User created: " + user.get("name"));
-        return "User Created!";
+    public ResponseEntity<?> createUser(@RequestBody Map<String, String> user) {
+        try {
+            System.out.println("MMMMMMMMMMMMMM");
+            // Send message and wait for ack
+            SendResult<String, String> result = kafkaTemplate.send("user-events", "User created: " + user.get("name"))
+                    .get(); // get() blocks
+
+            // Get RecordMetadata from SendResult
+            RecordMetadata metadata = result.getRecordMetadata();
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("message", "User Created!");
+            response.put("topic", metadata.topic());
+            response.put("partition", metadata.partition());
+            response.put("offset", metadata.offset());
+            System.out.println(response);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error sending message to Kafka: " + e.getMessage());
+        }
     }
+
 }
